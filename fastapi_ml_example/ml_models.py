9c298136bcadca9ccd16e6710b8d802388027a80
Bedarev Nikolay <nikolay.bedarev@yandex.ru>
2024-12-25 17:36:50 +0200

    update types

--- fastapi_ml_example/ml_models.py
+++ fastapi_ml_example/ml_models.py
@@ -1,16 +1,19 @@
 import asyncio
-import time
+import concurrent.futures
 import math
 import random
-import concurrent.futures
-import pickle
-import pathlib
+import time
+from typing import Protocol
 
 from fastapi_ml_example.schemes import PredictRequest
 
 
+class MLModel(Protocol):
+    def predict(self, **kwargs) -> float: ...
+
+
 class ExampleMLModel:
-    def predict(self, work_time: float) -> float:
+    def predict(self, work_time: float = 5, **kwargs) -> float:
         start = time.time()
         result = 0
         while time.time() - start < work_time:
@@ -19,27 +22,21 @@ class ExampleMLModel:
         return random.uniform(0, 1)
 
 
-_model: ExampleMLModel | None = None
+_model: MLModel | None = None
 
 
-def get_model() -> ExampleMLModel:
+def get_model() -> MLModel:
     global _model
     if _model is None:
-        path = pathlib.Path(__file__).parent.parent / "model.pkl"
-        if not path.exists():
-            raise ValueError("No model.pkl found, use `python ml_model_dump.py`")
-        with open(path, "rb") as fp:
-            _model = pickle.load(fp)
+        _model = ExampleMLModel()  # load model in memory
     return _model
 
 
 def _predict(request: PredictRequest) -> float:
     model = get_model()
-    return model.predict(work_time=request.work_time)
+    return model.predict(**request.model_dump())
 
 
-async def predict(
-    request: PredictRequest, executor: concurrent.futures.Executor
-) -> float:
+async def predict(request: PredictRequest, executor: concurrent.futures.Executor) -> float:
     event_loop = asyncio.get_event_loop()
     return await event_loop.run_in_executor(executor, _predict, request)
--- ml_model_dump.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-from fastapi_ml_example.ml_models import ExampleMLModel
-
-
-if __name__ == "__main__":
-    with open("model.pkl", "wb") as fp:
-        pickle.dump(ExampleMLModel(), fp)
