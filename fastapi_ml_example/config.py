c82cd8c919efa1eadd8c8072c067e9ba974e1651
Bedarev Nikolay <nikolay.bedarev@yandex.ru>
2024-12-25 23:14:18 +0200

    set default values

--- fastapi_ml_example/config.py
+++ fastapi_ml_example/config.py
@@ -7,15 +7,11 @@ import pydantic_settings
 class Settings(pydantic_settings.BaseSettings):
     """All service settings"""
 
-    project_name: str = pydantic.Field(
-        "fastapi-ml-example", validation_alias="PROJECT_NAME"
-    )
+    project_name: str = pydantic.Field("fastapi-ml-example", validation_alias="PROJECT_NAME")
     version: str = pydantic.Field("latest", validation_alias="VERSION")
     debug: bool = pydantic.Field(False, validation_alias="DEBUG")
 
-    process_pool_executor_size: int = pydantic.Field(
-        8, validation_alias="PROCESS_POOL_EXECUTOR_SIZE"
-    )
+    process_pool_executor_size: int = pydantic.Field(4, validation_alias="PROCESS_POOL_EXECUTOR_SIZE")
 
 
 def get_settings(settings: Optional[Settings] = None, **kwargs) -> Settings:
--- fastapi_ml_example/schemes/predictions.py
+++ fastapi_ml_example/schemes/predictions.py
@@ -2,7 +2,7 @@ import pydantic
 
 
 class PredictRequest(pydantic.BaseModel):
-    work_time: int
+    work_time: int = pydantic.Field(5)
 
 
 class PredictResponse(pydantic.BaseModel):
